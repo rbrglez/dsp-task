rbrglez/dsp-task

commit 25f3ac1704b96776d87965f2ccc3ffccfa693d66

VUnit: updated run.py script. fixed dsp_mac and matrix_vector_product, so that they can be compiled by GHDL

modules/matrix_vector_product/rtl/matrix_vector_product.vhd
@@ -81,12 +81,20 @@ architecture rtl of matrix_vector_product is
 
     signal in_dot_ready : std_logic_vector(NUM_DOT_PRODUCTS_G - 1 downto 0);
 
+    signal matrix_muxed : StlvArray_t(MATRIX_COLUMN_WIDTH_G - 1 downto 0)(IN_ELEMENT_WIDTH_G - 1 downto 0);
+
 begin
 
     ----------------------------------------------------------------------------
     -- Dot Product
     ----------------------------------------------------------------------------
     GEN_DOT_PRODUCT : for i in 0 to NUM_DOT_PRODUCTS_G - 1 generate
+
+        -- GHDL requires static names in port maps. 
+        -- Since r.matrix_extended(...) is dynamic, 
+        -- we use the intermediate signal matrix_muxed instead.
+        matrix_muxed <= r.matrix_extended(i + r.stage_idx * NUM_DOT_PRODUCTS_G);
+
         u_dot_product : entity work.dot_product
             generic map (
                 DIMENSION_WIDTH_G => MATRIX_COLUMN_WIDTH_G,
@@ -99,7 +107,7 @@ begin
 
                 in_valid_i    => r.in_dot_valid,
                 in_ready_o    => in_dot_ready(i),
-                in_vector_a_i => r.matrix_extended(i + r.stage_idx * NUM_DOT_PRODUCTS_G),
+                in_vector_a_i => matrix_muxed,
                 in_vector_b_i => r.vector,
 
                 out_valid_o       => out_dot_valid(i),

modules/dsp_mac/rtl/dsp_mac.vhd
@@ -1,7 +1,17 @@
+--------------------------------------------------------------------------------
+-- dsp_mac
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- Libraries
+--------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+--------------------------------------------------------------------------------
+-- Entity
+--------------------------------------------------------------------------------
 entity dsp_mac is
     generic (
         WIDTH_A_G : natural := 16;
@@ -19,9 +29,6 @@ entity dsp_mac is
 end entity dsp_mac;
 
 architecture rtl of dsp_mac is
-
-    attribute use_dsp : string;
-    attribute use_dsp of p_o : signal is "yes";
 
 begin
     process(clk_i)

sim/run.py
@@ -138,35 +138,23 @@ vu.add_verification_components()
 
 # Create a library
 olo = vu.add_library('olo')
-#olo_tb = vu.add_library('olo_tb')
-#dsp_task = vu.add_library('dsp_task')
-#dsp_task_tb = vu.add_library('dsp_task_tb')
-
 lib = vu.add_library('lib')
 
 # Add all open-logic VHDL files
 files  = glob('../submodules/open-logic/src/**/*.vhd', recursive=True)
 files += glob('../submodules/open-logic/3rdParty/en_cl_fix/hdl/*.vhd', recursive=True)
 olo.add_source_files(files)
-#lib.add_source_files(files)
 
 # Add all source VHDL files
-#files  = glob('../modules/**/rtl/*.vhd', recursive=True)
-files  = glob('../modules/fix_dsp_mac/rtl/*.vhd', recursive=True)
-files += glob('../modules/fix_dot_product/rtl/*.vhd', recursive=True)
-#dsp_task.add_source_files(files)
+files = glob('../modules/**/rtl/*.vhd', recursive=True)
 lib.add_source_files(files)
 
 # Add test helpers
 files = glob('../submodules/open-logic/test/tb/*.vhd', recursive=True)
-#olo_tb.add_source_files(files)
 lib.add_source_files(files)
 
 # Add all vunit tb VHDL files
-#files += glob('../modules/**/vunit_tb/*.vhd', recursive=True)
-files  = glob('../modules/fix_dsp_mac/vunit_tb/*.vhd', recursive=True)
-files += glob('../modules/fix_dot_product/vunit_tb/*.vhd', recursive=True)
-#dsp_task_tb.add_source_files(files)
+files  = glob('../modules/**/vunit_tb/*.vhd', recursive=True)
 lib.add_source_files(files)
 
 # Obviously flags must be set after files are imported

commit c57141c14b2748ac2ed01445892db6bed42c0caa

matrix_vector_product: fix regression introduced in commit 25f3ac1704b9

modules/matrix_vector_product/rtl/matrix_vector_product.vhd
@@ -81,19 +81,19 @@ architecture rtl of matrix_vector_product is
 
     signal in_dot_ready : std_logic_vector(NUM_DOT_PRODUCTS_G - 1 downto 0);
 
-    signal matrix_muxed : StlvArray_t(MATRIX_COLUMN_WIDTH_G - 1 downto 0)(IN_ELEMENT_WIDTH_G - 1 downto 0);
+    signal matrix_muxed : StlvVectorArray_t(NUM_DOT_PRODUCTS_G - 1 downto 0)(MATRIX_COLUMN_WIDTH_G - 1 downto 0)(IN_ELEMENT_WIDTH_G - 1 downto 0);
 
 begin
+
+    -- GHDL requires static names in port maps. 
+    -- Since r.matrix_extended(...) is dynamic, 
+    -- we use the intermediate signal matrix_muxed instead.
+    matrix_muxed <= r.matrix_extended((r.stage_idx + 1) * NUM_DOT_PRODUCTS_G - 1 downto r.stage_idx * NUM_DOT_PRODUCTS_G);
 
     ----------------------------------------------------------------------------
     -- Dot Product
     ----------------------------------------------------------------------------
     GEN_DOT_PRODUCT : for i in 0 to NUM_DOT_PRODUCTS_G - 1 generate
-
-        -- GHDL requires static names in port maps. 
-        -- Since r.matrix_extended(...) is dynamic, 
-        -- we use the intermediate signal matrix_muxed instead.
-        matrix_muxed <= r.matrix_extended(i + r.stage_idx * NUM_DOT_PRODUCTS_G);
 
         u_dot_product : entity work.dot_product
             generic map (
@@ -107,7 +107,7 @@ begin
 
                 in_valid_i    => r.in_dot_valid,
                 in_ready_o    => in_dot_ready(i),
-                in_vector_a_i => matrix_muxed,
+                in_vector_a_i => matrix_muxed(i),
                 in_vector_b_i => r.vector,
 
                 out_valid_o       => out_dot_valid(i),