06c5eb6a344bbbe747ea12a56db7486d8a9ac3a6
Rene Brglez <rene.brglez@gmail.com>
2025-10-31 20:24:32 +0100

    Updated vunit run.py

--- sim/run.py
+++ sim/run.py
@@ -1,6 +1,6 @@
-########################################################################################################################
+################################################################################
 # Imports
-########################################################################################################################
+################################################################################
 from vunit import VUnit
 from glob import glob
 import os
@@ -8,127 +8,12 @@ import sys
 from enum import Enum
 from functools import partial
 
-## TODO: create test configuration file
+# Import vunit test configurations
+from test_configs import modules
 
 ################################################################################
-## utils.py
-################################################################################
-
-# ---------------------------------------------------------------------------------------------------
-# Functionality
-# ---------------------------------------------------------------------------------------------------
-def named_config(tb, map : dict, pre_config = None, short_name = None):
-    cfg_name = "-".join([f"{k}={v}" for k, v in map.items()])
-    if short_name is not None:
-        cfg_name = short_name
-    if pre_config is not None:
-        pre_config = partial(pre_config, generics=map)
-    tb.add_config(name=cfg_name, generics = map, pre_config=pre_config)
-
-def make_short_name(generics, generic_aliases):
-    parts = []
-    for k, v in generics.items():
-        alias = generic_aliases.get(k, k)  # use alias if defined, else full key
-        parts.append(f"{alias}={v}")
-    return "_".join(parts)
-################################################################################
-## test_configs.py
-################################################################################
-
-# ---------------------------------------------------------------------------------------------------
-# Imports
-# ---------------------------------------------------------------------------------------------------
-#from .utils import named_config
-import sys
-import os
-
-# Import for fix cosimulations
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from modules import *
-
-# ---------------------------------------------------------------------------------------------------
-# Functionality
-# ---------------------------------------------------------------------------------------------------
-
-def add_configs(lib):
-    """
-    Add all base testbench configurations to the VUnit Library
-    :param lib: Testbench library
-    """
-
-    ### fix_dsp_mac ###
-    tb = lib.test_bench('fix_dsp_mac_vunit_tb')
-    default_generics = {
-        'FMT_MULT_A_G' : '(0,4,4)',
-        'FMT_MULT_B_G' : '(0,4,4)',
-        'FMT_ADD_G'    : '(0,8,8)',
-        'FMT_RESULT_G' : '(0,9,8)',
-    }
-
-    cosim = fix_dsp_mac.vunit_tb.cosim.cosim
-
-    named_config(tb, default_generics, pre_config=cosim)
-
-    ### fix_dot_product ###
-    tb = lib.test_bench('fix_dot_product_vunit_tb')
-    default_generics = {
-        'DIMENSION_WIDTH_G' : 4,
-        'FMT_IN_ELEMENT_A_G': '(0,4,4)',
-        'FMT_IN_ELEMENT_B_G': '(0,4,4)',
-        'FMT_OUT_RESULT_G'  : '(0,10,8)',
-    }
-
-    cosim = fix_dot_product.vunit_tb.cosim.cosim
-
-    named_config(tb, default_generics, pre_config=cosim)
-
-    ### fix_matrix_vector_product ###
-    tb = lib.test_bench('fix_matrix_vector_product_vunit_tb')
-    cosim = fix_matrix_vector_product.vunit_tb.cosim.cosim
-
-    generic_aliases = {
-        'COSIM_NUM_TEST_VECTORS_G' : 'NUM_SAMPLES',
-        'COSIM_MATRIX_TYPE_G'      : 'MATRIX_TYPE',
-        'NUM_DOT_PRODUCTS_G'       : 'DSPs',
-        'MATRIX_ROW_WIDTH_G'       : 'ROWs',
-        'MATRIX_COLUMN_WIDTH_G'    : 'COLs',
-        'FMT_IN_MATRIX_ELEMENT_G'  : 'FMT_MATRIX',
-        'FMT_IN_VECTOR_ELEMENT_G'  : 'FMT_VECTOR',
-        'FMT_OUT_RESULT_G'         : 'FMT_RESULT',
-    }
-
-    default_generics = {
-        'COSIM_NUM_TEST_VECTORS_G' : 16,
-        'COSIM_MATRIX_TYPE_G'      : 'DESCENDING',
-        'NUM_DOT_PRODUCTS_G'       : 11,
-        'MATRIX_ROW_WIDTH_G'       : 32,
-        'MATRIX_COLUMN_WIDTH_G'    : 32,
-        'FMT_IN_MATRIX_ELEMENT_G'  : '(0,8,8)',
-        'FMT_IN_VECTOR_ELEMENT_G'  : '(0,8,8)',
-        'FMT_OUT_RESULT_G'         : '(0,21,16)',
-    }
-
-    short_name = make_short_name(default_generics, generic_aliases)
-    named_config(tb, default_generics, pre_config=cosim, short_name=short_name)
-
-    default_generics = {
-        'COSIM_NUM_TEST_VECTORS_G' : 8,
-        'COSIM_MATRIX_TYPE_G'      : 'ASCENDING',
-        'NUM_DOT_PRODUCTS_G'       : 2,
-        'MATRIX_ROW_WIDTH_G'       : 4,
-        'MATRIX_COLUMN_WIDTH_G'    : 4,
-        'FMT_IN_MATRIX_ELEMENT_G'  : '(0,4,1)',
-        'FMT_IN_VECTOR_ELEMENT_G'  : '(0,4,1)',
-        'FMT_OUT_RESULT_G'         : '(0,10,2)',
-    }
-
-    # Build a compact short name
-    short_name = make_short_name(default_generics, generic_aliases)
-    named_config(tb, default_generics, pre_config=cosim, short_name=short_name)
-
-########################################################################################################################
 # Setup
-########################################################################################################################
+################################################################################
 
 class Simulator(Enum):
     GHDL = 1
@@ -209,16 +94,16 @@ lib.add_source_files(files)
 vu.add_compile_option('ghdl.a_flags', ['-frelaxed-rules', '-Wno-hide', '-Wno-shared'])
 vu.add_compile_option('nvc.a_flags', ['--relaxed'])
 
-########################################################################################################################
+################################################################################
 # Test bench configurations
-########################################################################################################################
+################################################################################
 
 ## Defined at the top of this file!
-add_configs(lib)
+modules.add_configs(lib)
 
-########################################################################################################################
+################################################################################
 # Execution
-########################################################################################################################
+################################################################################
 
 lib.set_sim_option('ghdl.elab_flags', ['-frelaxed'])
 lib.set_sim_option('nvc.heap_size', '5000M')
---
+++ sim/test_configs/modules.py
@@ -0,0 +1,141 @@
+################################################################################
+# modules.py
+################################################################################
+
+################################################################################
+# Imports
+################################################################################
+from .utils import named_config, make_short_name
+import sys
+import os
+
+# Import for fix cosimulations
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+from modules import *
+
+################################################################################
+# Functionality
+################################################################################
+
+def add_configs(lib):
+    """
+    Add all base testbench configurations to the VUnit Library
+    :param lib: Testbench library
+    """
+
+    ############################################################################
+    # fix_dsp_mac 
+    ############################################################################
+    tb = lib.test_bench('fix_dsp_mac_vunit_tb')
+    cosim = fix_dsp_mac.vunit_tb.cosim.cosim
+
+    generics = {
+        'FMT_MULT_A_G' : '(0,4,4)',
+        'FMT_MULT_B_G' : '(0,4,4)',
+        'FMT_ADD_G'    : '(0,8,8)',
+        'FMT_RESULT_G' : '(0,9,8)',
+    }
+    named_config(tb, generics, pre_config=cosim)
+
+    generics = {
+        'FMT_MULT_A_G' : '(1,5,8)',
+        'FMT_MULT_B_G' : '(1,7,9)',
+        'FMT_ADD_G'    : '(1,13,17)',
+        'FMT_RESULT_G' : '(1,14,17)',
+    }
+    named_config(tb, generics, pre_config=cosim)
+
+    generics = {
+        'FMT_MULT_A_G' : '(0,11,-4)',
+        'FMT_MULT_B_G' : '(1,6,2)',
+        'FMT_ADD_G'    : '(0,4,5)',
+        'FMT_RESULT_G' : '(1,17,5)',
+    }
+    named_config(tb, generics, pre_config=cosim)
+
+    ############################################################################
+    # fix_dot_product
+    ############################################################################
+    tb = lib.test_bench('fix_dot_product_vunit_tb')
+    cosim = fix_dot_product.vunit_tb.cosim.cosim
+
+    generics = {
+        'DIMENSION_WIDTH_G' : 4,
+        'FMT_IN_ELEMENT_A_G': '(0,4,4)',
+        'FMT_IN_ELEMENT_B_G': '(0,4,4)',
+        'FMT_OUT_RESULT_G'  : '(0,10,8)',
+    }
+    named_config(tb, generics, pre_config=cosim)
+
+    generics = {
+        'DIMENSION_WIDTH_G' : 32,
+        'FMT_IN_ELEMENT_A_G': '(1,3,2)',
+        'FMT_IN_ELEMENT_B_G': '(1,5,8)',
+        'FMT_OUT_RESULT_G'  : '(1,13,10)',
+    }
+    named_config(tb, generics, pre_config=cosim)
+
+    generics = {
+        'DIMENSION_WIDTH_G' : 100,
+        'FMT_IN_ELEMENT_A_G': '(1,7,-2)',
+        'FMT_IN_ELEMENT_B_G': '(0,2,8)',
+        'FMT_OUT_RESULT_G'  : '(0,16,6)',
+    }
+    named_config(tb, generics, pre_config=cosim)
+
+    ############################################################################
+    # fix_matrix_vector_product
+    ############################################################################
+    tb = lib.test_bench('fix_matrix_vector_product_vunit_tb')
+    cosim = fix_matrix_vector_product.vunit_tb.cosim.cosim
+
+    ## Need to shorten generics, because of file name limit
+    generic_aliases = {
+        'COSIM_NUM_TEST_VECTORS_G' : 'NUM_SAMPLES',
+        'COSIM_MATRIX_TYPE_G'      : 'MATRIX_TYPE',
+        'NUM_DOT_PRODUCTS_G'       : 'DSPs',
+        'MATRIX_ROW_WIDTH_G'       : 'ROWs',
+        'MATRIX_COLUMN_WIDTH_G'    : 'COLs',
+        'FMT_IN_MATRIX_ELEMENT_G'  : 'FMT_MATRIX',
+        'FMT_IN_VECTOR_ELEMENT_G'  : 'FMT_VECTOR',
+        'FMT_OUT_RESULT_G'         : 'FMT_RESULT',
+    }
+
+    generics = {
+        'COSIM_NUM_TEST_VECTORS_G' : 16,
+        'COSIM_MATRIX_TYPE_G'      : 'DESCENDING',
+        'NUM_DOT_PRODUCTS_G'       : 11,
+        'MATRIX_ROW_WIDTH_G'       : 32,
+        'MATRIX_COLUMN_WIDTH_G'    : 32,
+        'FMT_IN_MATRIX_ELEMENT_G'  : '(0,4,12)',
+        'FMT_IN_VECTOR_ELEMENT_G'  : '(0,4,12)',
+        'FMT_OUT_RESULT_G'         : '(0,13,24)',
+    }
+    short_name = make_short_name(generics, generic_aliases)
+    named_config(tb, generics, pre_config=cosim, short_name=short_name)
+
+    generics = {
+        'COSIM_NUM_TEST_VECTORS_G' : 8,
+        'COSIM_MATRIX_TYPE_G'      : 'ASCENDING',
+        'NUM_DOT_PRODUCTS_G'       : 2,
+        'MATRIX_ROW_WIDTH_G'       : 4,
+        'MATRIX_COLUMN_WIDTH_G'    : 4,
+        'FMT_IN_MATRIX_ELEMENT_G'  : '(0,8,1)',
+        'FMT_IN_VECTOR_ELEMENT_G'  : '(0,3,5)',
+        'FMT_OUT_RESULT_G'         : '(0,13,6)',
+    }
+    short_name = make_short_name(generics, generic_aliases)
+    named_config(tb, generics, pre_config=cosim, short_name=short_name)
+
+    generics = {
+        'COSIM_NUM_TEST_VECTORS_G' : 10,
+        'COSIM_MATRIX_TYPE_G'      : 'DESCENDING',
+        'NUM_DOT_PRODUCTS_G'       : 5,
+        'MATRIX_ROW_WIDTH_G'       : 17,
+        'MATRIX_COLUMN_WIDTH_G'    : 4,
+        'FMT_IN_MATRIX_ELEMENT_G'  : '(0,3,8)',
+        'FMT_IN_VECTOR_ELEMENT_G'  : '(0,8,4)',
+        'FMT_OUT_RESULT_G'         : '(0,13,12)',
+    }
+    short_name = make_short_name(generics, generic_aliases)
+    named_config(tb, generics, pre_config=cosim, short_name=short_name)
