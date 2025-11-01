266c404a60308404ff4d21f45a90977fcedf38f7
Rene Brglez <rene.brglez@gmail.com>
2025-11-01 21:41:36 +0100

    fix_dsp_mac: Add generics for Multiplication and Addition unit. Add comments

--- modules/fix_dsp_mac/rtl/fix_dsp_mac.vhd
+++ modules/fix_dsp_mac/rtl/fix_dsp_mac.vhd
@@ -15,14 +15,26 @@ use olo.en_cl_fix_pkg.all;
 use olo.olo_fix_pkg.all;
 
 --------------------------------------------------------------------------------
--- Entitity
+-- Entity
 --------------------------------------------------------------------------------
 entity fix_dsp_mac is
     generic(
         FMT_MULT_A_G : string := "(0, 2, 2)";
         FMT_MULT_B_G : string := "(0, 2, 2)";
         FMT_ADD_G    : string := "(0, 4, 4)";
-        FMT_RESULT_G : string := "(0, 5, 4)"
+        FMT_RESULT_G : string := "(0, 5, 4)";
+
+        MULT_ROUND_G     : string  := FixRound_Trunc_c;
+        MULT_SATURATE_G  : string  := FixSaturate_Warn_c;
+        MULT_OP_REGS_G   : natural := 0;
+        MULT_ROUND_REG_G : string  := "NO";
+        MULT_SAT_REG_G   : string  := "NO";
+
+        ADD_ROUND_G     : string  := FixRound_Trunc_c;
+        ADD_SATURATE_G  : string  := FixSaturate_Warn_c;
+        ADD_OP_REGS_G   : natural := 1;
+        ADD_ROUND_REG_G : string  := "NO";
+        ADD_SAT_REG_G   : string  := "NO"
     );
     port (
         clk_i : in std_logic;
@@ -46,11 +58,17 @@ end entity fix_dsp_mac;
 
 architecture rtl of fix_dsp_mac is
 
+    ----------------------------------------------------------------------------
+    -- Constants
+    ----------------------------------------------------------------------------
     constant FIX_FMT_MULT_A_C : FixFormat_t := cl_fix_format_from_string(FMT_MULT_A_G);
     constant FIX_FMT_MULT_B_C : FixFormat_t := cl_fix_format_from_string(FMT_MULT_B_G);
 
     constant FMT_MULT_RESULT_C : string := to_string(cl_fix_mult_fmt(FIX_FMT_MULT_A_C, FIX_FMT_MULT_B_C));
 
+    ----------------------------------------------------------------------------
+    -- Instantiation signals
+    ----------------------------------------------------------------------------
     signal mult_valid  : std_logic;
     signal mult_result : std_logic_vector(fixFmtWidthFromString(FMT_MULT_RESULT_C) - 1 downto 0);
 
@@ -61,11 +79,11 @@ begin
             AFmt_g      => FMT_MULT_A_G,
             BFmt_g      => FMT_MULT_B_G,
             ResultFmt_g => FMT_MULT_RESULT_C,
-            Round_g     => "Trunc_s",
-            Saturate_g  => "Warn_s",
-            OpRegs_g    => 0,
-            RoundReg_g  => "NO",
-            SatReg_g    => "NO"
+            Round_g     => MULT_ROUND_G,
+            Saturate_g  => MULT_SATURATE_G,
+            OpRegs_g    => MULT_OP_REGS_G,
+            RoundReg_g  => MULT_ROUND_REG_G,
+            SatReg_g    => MULT_SAT_REG_G
         )
         port map (
             Clk => clk_i,
@@ -84,11 +102,11 @@ begin
             AFmt_g      => FMT_MULT_RESULT_C,
             BFmt_g      => FMT_ADD_G,
             ResultFmt_g => FMT_RESULT_G,
-            Round_g     => "Trunc_s",
-            Saturate_g  => "Warn_s",
-            OpRegs_g    => 1,
-            RoundReg_g  => "NO",
-            SatReg_g    => "NO"
+            Round_g     => ADD_ROUND_G,
+            Saturate_g  => ADD_SATURATE_G,
+            OpRegs_g    => ADD_OP_REGS_G,
+            RoundReg_g  => ADD_ROUND_REG_G,
+            SatReg_g    => ADD_SAT_REG_G
         )
         port map (
             Clk => clk_i,
